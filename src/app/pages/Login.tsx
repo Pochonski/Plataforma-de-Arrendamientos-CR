31160d8a9bd494521cf4704e1af64b9898991b20
Joseph Fonseca <joseph19102005@gmail.com>
2026-04-16 21:21:07 -0600

    update

--- src/app/pages/Login.tsx
+++ src/app/pages/Login.tsx
@@ -173,7 +173,6 @@ export default function Login() {
                   onError={() => {
                     toast.error('Error con Google OAuth');
                   }}
-                  useOneTap
                 />
               </div>
 
